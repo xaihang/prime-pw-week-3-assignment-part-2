0d36c728863d2220de40fdb1ae3db2bac81278c9
Xai Hang <xaihang12@gmail.com>
2022-12-12 13:16:08 -0600

    q3c wrote while loop for counting from 10 to 5 backwards

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -56,10 +56,15 @@ console.log('---- 3. While loop ----');
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 let count = 0;
-while(count < 5 ){
+while (count < 5) {
   console.log(count);
   count++;
 }
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let countDown = 10;
+while (countDown >= 5) {
+  console.log(countDown);
+  countDown--;
+}
