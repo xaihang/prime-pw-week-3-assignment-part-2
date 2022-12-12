618046a8af01109aa81ca2279896f84440c714ed
Xai Hang <xaihang12@gmail.com>
2022-12-12 12:59:46 -0600

    q3b wrote while loop counting from 0 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -44,7 +44,7 @@ console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
-for (i=0; i<stars.length; i++){
+for (i = 0; i < stars.length; i++) {
   console.log('Some stars:', stars[i]);
 }
 
@@ -52,10 +52,14 @@ for (i=0; i<stars.length; i++){
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-console.log('Some stars using while:');
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let count = 0;
+while(count < 5 ){
+  console.log(count);
+  count++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
