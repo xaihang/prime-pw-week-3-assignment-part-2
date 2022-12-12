ce16b006860332f0ff0d714a97423e2a03801f3f
Xai Hang <xaihang12@gmail.com>
2022-12-12 11:58:08 -0600

    q1a wrote for loop to count from 0 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -12,7 +12,11 @@ for (let i=0; i<4; i++) {
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
+// answer: change the expression part 2, or condition (while i is < 6).
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++){
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
