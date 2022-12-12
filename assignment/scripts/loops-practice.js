421772653373a226befeef6e84630cc1ab653515
Xai Hang <xaihang12@gmail.com>
2022-12-12 12:22:11 -0600

    q2a wrote a for loop for each star in the stars array

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -44,7 +44,9 @@ console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
-console.log('Some stars:');
+for (i=0; i<stars.length; i++){
+  console.log('Some stars:', stars[i]);
+}
 
 // 3. While loops
 console.log('---- 3. While loop ----');
